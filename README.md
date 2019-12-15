c3d9a33797e91fba746397c18117bc193a3b5f00
Tim McMaster <tim@mcmaster.id.au>
2019-12-16 09:59:36 +1100

    init

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# template-web-component
+# tm-page-layout
 
 This is a template for creating new web component repositories.
 
 ## Installation
 ```bash
-npm install @wonkytech/template-web-component
+npm install @wonkytech/tm-page-layout
 ```
 
 ## Usage
 ```html
 <script type="module">
-  import '@wonkytech/template-web-component';
+  import '@wonkytech/tm-page-layout';
 </script>
 
-<template-web-component></template-web-component>
+<tm-page-layout></tm-page-layout>
 ```
--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,10 @@
 import {html, render} from "./web_modules/lit-html.js";
 
 let sites = {
-    'src': 'https://github.com/tmcmaster/template-web-component',
-    'pika': 'https://www.pika.dev/npm/@wonkytech/template-web-component',
-    'npm': 'https://www.npmjs.com/package/@wonkytech/template-web-component',
-    'docs': 'https://github.com/tmcmaster/template-web-component#readme'
+    'src': 'https://github.com/tmcmaster/tm-page-layout',
+    'pika': 'https://www.pika.dev/npm/@wonkytech/tm-page-layout',
+    'npm': 'https://www.npmjs.com/package/@wonkytech/tm-page-layout',
+    'docs': 'https://github.com/tmcmaster/tm-page-layout#readme'
 };
 
 render(html`
@@ -14,9 +14,9 @@ render(html`
           margin: 0;
         } 
     </style>
-    <tm-examples heading="template-web-component" .sites="${sites}">
+    <tm-examples heading="tm-page-layout" .sites="${sites}">
         <section title="Example">
-            <template-web-component></template-web-component>
+            <tm-page-layout></tm-page-layout>
         </section>
     </tm-examples>
 `, document.querySelector('body'));
